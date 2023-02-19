de0cc27987189ff5703dbb0d855ad61e75831055
Yehor Boichenko <boichenko.yehor@gmail.com>
2023-02-19 12:47:02 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { UserEntity } from '@app/user/user.entity';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mysql',
-        host: 'localhost',
-        port:  3306,
-        username: 'root',
-        password: '1234',
-        database: 'form',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [ UserEntity], //entities: [__dirname + '/**/ */.entity{.ts,.js}']
         synchronize: true,
         ssl: true,
